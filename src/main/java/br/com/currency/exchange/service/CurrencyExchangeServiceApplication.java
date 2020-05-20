1b4ed8299959aabd4e859c67957a66ab14ad4abf
Diego Gean da Fé <diegogeandafe@gmail.com>
2020-05-20 20:58:28 -0300

    Atualizado pom.xml

--- src/main/java/br/com/currency/exchange/service/CurrencyExchangeServiceApplication.java
+++ src/main/java/br/com/currency/exchange/service/CurrencyExchangeServiceApplication.java
@@ -3,12 +3,14 @@ package br.com.currency.exchange.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 
 import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
